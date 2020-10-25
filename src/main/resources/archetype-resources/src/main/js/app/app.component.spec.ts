6cf32a659b85e912d3c53d681a6606859cec6ebc
Bernardo <programming@bernardomg.com>
2020-10-25 22:30:35 +0100

    Replaced project name on frontend

--- src/main/resources/archetype-resources/src/main/js/app/app.component.spec.ts
+++ src/main/resources/archetype-resources/src/main/js/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'dnd5-api-angular-client'`, () => {
+  it(`should have as title '${artifactId}'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('dnd5-api-angular-client');
+    expect(app.title).toEqual('${artifactId}');
   });
 
   // it('should render title', () => {
   //   const fixture = TestBed.createComponent(AppComponent);
   //   fixture.detectChanges();
   //   const compiled = fixture.nativeElement;
-  //   expect(compiled.querySelector('.content span').textContent).toContain('dnd5-api-angular-client app is running!');
+  //   expect(compiled.querySelector('.content span').textContent).toContain('${artifactId} app is running!');
   // });
 });
